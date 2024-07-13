1f4c2b8f4aaffa47f9fcae14676d21bcc6244124
Marvelous <marvelous.udugbesi@icloud.com>
2024-07-13 19:15:21 +0100

    refactoring code

--- src/api/timbleApi.js
+++ src/api/timbleApi.js
@@ -4,45 +4,6 @@ const timbleApiKey = import.meta.env.VITE_TIMBLE_API_KEY
 const timbleOrgId = import.meta.env.VITE_TIMBLE_ORG_ID
 
 
-// export const handleApi = async () => {
-//     try {
-//         const response = await fetch(`/api/products?organization_id=${timbleOrgId}&reverse_sort=false&Appid=${timbleAppId}&Apikey=${timbleApiKey}`)
-
-//         if (response.ok) {
-//             const data = await response.json();
-//             return data.items;
-//         } else {
-//             throw new Error('Network response was not ok');
-//         }
-
-//     } catch (e) {
-//         console.error(e.message)
-//     }
-
-// }
-
-// export const handleSalesApi = async (props) => {
-//     try {
-//         const response = await fetch(`/api/sales?organization_id=${timbleOrgId}&Appid=${timbleAppId}&Apikey=${timbleApiKey}`, {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify(props)
-//         });
-
-//         if (response.ok) {
-//             return response
-//         } else {
-//             alert('error')
-//         }
-
-//     } catch (e) {
-//         throw new Error('Error: ', e.message)
-//     }
-// }
-
-
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -53,7 +14,7 @@ export const handleApi = async () => {
         // Displaying loading toast
         toast.info('Fetching products...', { autoClose: false });
 
-        const response = await fetch(`/api/products?organization_id=${timbleOrgId}&reverse_sort=false&Appid=${timbleAppId}&Apikey=${timbleApiKey}`);
+        const response = await fetch(`https://timbu-get-all-products.reavdev.workers.dev/?organization_id=${timbleOrgId}&reverse_sort=false&Appid=${timbleAppId}&Apikey=${timbleApiKey}`);
 
         if (response.ok) {
             const data = await response.json();
